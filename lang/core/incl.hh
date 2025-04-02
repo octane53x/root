1e92aaf062c8f1ed137c0980e12e7704407b9b6d
octane53x <octane53x@gmail.com>
2025-04-02 02:09:31 -0600

    Create incl.hh

---
+++ lang/core/incl.hh
@@ -0,0 +1,12 @@
+// incl.hh
+
+#include <queue>
+#include <deque>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+#define ll long long
+#define ull unsigned long long
+#define vec vector
+#define umap unordered_map
