2684dcecfaf98abda1a5a886916e496f176a03f2
Ryan Bell <octane53x@gmail.com>
2025-12-16 11:45:18 -0700

    editor types characters but displays wrong

--- edit/editor.hh
+++ edit/editor.hh
@@ -3,14 +3,19 @@
 #ifndef EDITOR_HH
 #define EDITOR_HH
 
+#include "../os/win/util.hh"
 #include "../os/win/window.hh"
 #include "text_view.hh"
 
-int scene::win_w, scene::win_h;
-umap<str, font> scene::fonts;
+const str
+    _FONT_LOC = "../symbols.bmp",
+    _SYMBOLS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        ".,/?:;'\"+=-_\\|~!@#$%^&*()[]{}<>";
 
 struct Editor : virtual window {
 
+  bool shift, ctrl, alt;
+  umap<char, image> font;
   TextView scene_text;
 
   Editor();
@@ -19,13 +24,15 @@ struct Editor : virtual window {
   virtual void run();
   virtual void update(const double ms);
 
+  void load_font();
   void process_key(env::key_event ke); };
 
-Editor::Editor(){
+Editor::Editor(): shift(false), ctrl(false), alt(false) {
   type = "Editor"; }
 
 void Editor::init(){
-  //! Load font
+  load_font();
+  scene_text.font = font;
   scene_text.init();
   window::init(); }
 
@@ -41,8 +48,46 @@ void Editor::update(const double ms){
   window::update(ms);
   last_update = clock(); }
 
+void Editor::load_font(){
+  image font_img = load_bmp(_FONT_LOC);
+  assert(font_img.width == _SYMBOLS.size() * CHAR_WIDTH, "Editor.load_font",
+      "font image not aligned with symbol list");
+  for(int i = 0; i < _SYMBOLS.size(); ++i){
+    image new_char(CHAR_WIDTH, LINE_HEIGHT);
+    for(int xi = i * CHAR_WIDTH, xo = 0; xo < CHAR_WIDTH; ++xi, ++xo)
+      for(int y = 0; y < LINE_HEIGHT; ++y)
+        new_char.set_pixel(xo, y, font_img.data[y][xi]);
+    font[_SYMBOLS[i]] = new_char; } }
+
 void Editor::process_key(env::key_event ke){
-  //! Key input
+  // 0-9, A-Z, a-z
+  if(ke.key.size() == 1){
+    char ci = ke.key[0], co;
+    if(ci >= '0' && ci <= '9')
+      co = ci;
+    else{
+      assert(ci >= 'A' && ci <= 'Z', "Editor.process_key", "bad character");
+      if(shift)
+        co = ci;
+      else
+        co = ci - 'A' + 'a'; }
+    str line = scene_text.text[scene_text.line_pos];
+    scene_text.text[scene_text.line_pos] = line.substr(0, scene_text.char_pos)
+        + str("" + co) + line.substr(scene_text.char_pos);
+    ++scene_text.char_pos;
+
+  // Modifiers
+  }else if(ke.key == "SHIFT")
+    shift = ke.down;
+  else if(ke.key == "CONTROL")
+    ctrl = ke.down;
+  else if(ke.key == "ALT")
+    alt = ke.down;
+
+  //! more
 }
+
+int scene::win_w, scene::win_h;
+umap<str, font> scene::fonts;
 
 #endif
--- edit/text_view.hh
+++ edit/text_view.hh
@@ -18,13 +18,19 @@ struct TextView : virtual scene {
     virtual void update(const double ms);
   } cursor;
 
+  int line_pos, char_pos;
+  vec<str> text;
+  umap<char, image> font;
+
   TextView();
 
   virtual void init();
-  virtual void update(const double ms); };
+  virtual void update(const double ms);
+  virtual void draw(image* canvas, const viewport& view); };
 
-TextView::TextView(){
-  type = "TextView"; }
+TextView::TextView(): line_pos(0), char_pos(0) {
+  type = "TextView";
+  text.pb(""); }
 
 void TextView::init(){
   width = win_w, height = win_h;
@@ -41,12 +47,20 @@ void TextView::init(){
   scene::init(); }
 
 void TextView::update(const double ms){
-  scene::update(ms); }
+  scene::update(ms);
+  cursor.pos = point(char_pos * CHAR_WIDTH, line_pos * LINE_HEIGHT); }
+
+void TextView::draw(image* canvas, const viewport& view){
+  scene::draw(canvas, view);
+  for(int y = 0; y < text.size(); ++y)
+    for(int x = 0; x < text[y].size(); ++x){
+      image c = font[text[y][x]];
+      c.pos = point(x * CHAR_WIDTH, y * LINE_HEIGHT);
+      c.draw(canvas, view); } }
 
 TextView::Cursor::Cursor(): blink(true) {}
 
 void TextView::Cursor::update(const double ms){
-  debug("Cursor.update");
   double sec = (double)(clock() - last_update) / CLOCKS_PER_SEC;
   if(sec >= CURSOR_BLINK){
     blink = !blink;
