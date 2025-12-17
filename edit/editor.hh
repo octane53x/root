a12357277ea6a68f7a8ee87fb65a6783c22a447c
Ryan Bell <octane53x@gmail.com>
2025-12-17 12:20:55 -0700

    moved window code out of editor file

--- edit/editor.hh
+++ edit/editor.hh
@@ -5,6 +5,7 @@
 
 #include "../os/win/util.hh" // load_bmp
 #include "../gl/polygon.hh"
+#include "window.hh"
 
 const int
     INIT_WIN_W = 1500,
@@ -21,18 +22,11 @@ const str
     _SYMBOLS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
         ".,/?:;'\"+=-_\\|~!@#$%^&*()[]{}<>";
 
-struct Editor {
+struct Editor : virtual window {
 
-  HINSTANCE win_param_1;
-  int win_param_2;
-
-  bool updated, refresh_line, refresh_page, shift, ctrl, alt;
-  int width, height;
-  clock_t last_update;
+  bool refresh_line, refresh_page, shift, ctrl, alt;
   color bkgd_color;
-  point win_pos, mouse_pos;
   vec<str> text;
-  image frame;
   umap<char, image> font;
   viewport view;
 
@@ -49,105 +43,16 @@ struct Editor {
   void draw_bkgd();
   void load_font();
   void process_key(const str& key, const bool down, const point& mouse);
-  void add_char(const char c);
-} edit;
-
-void _win_paint(HWND hwnd){
-  if(!edit.updated) return;
-  PAINTSTRUCT ps;
-  HDC hdc = BeginPaint(hwnd, &ps);
-  HBITMAP bmp = image_to_bmp(hdc, &edit.frame);
-  HDC hdcMem = CreateCompatibleDC(NULL);
-  HBITMAP bmpPrev = (HBITMAP)SelectObject(hdcMem, bmp);
-  BitBlt(hdc, 0, 0, edit.width, edit.height, hdcMem, 0, 0, SRCCOPY);
-  SelectObject(hdcMem, bmpPrev);
-  DeleteDC(hdcMem);
-  EndPaint(hwnd, &ps);
-  edit.updated = false; }
-
-str _win_key(WPARAM wParam){
-  str s;
-  switch(wParam){
-    // Mouse clicks not handled like this:
-    // case VK_LBUTTON: s = "LCLICK"; break;
-    // case VK_RBUTTON: s = "RCLICK"; break;
-    // case VK_MBUTTON: s = "MCLICK"; break;
-    case VK_BACK: s = "BACKSPACE"; break;
-    case VK_TAB: s = "TAB"; break;
-    case VK_RETURN: s = "ENTER"; break;
-    case VK_SHIFT: s = "SHIFT"; break;
-    case VK_CONTROL: s = "CONTROL"; break;
-    case VK_MENU: s = "ALT"; break;
-    case VK_CAPITAL: s = "CAPSLOCK"; break;
-    case VK_ESCAPE: s = "ESCAPE"; break;
-    case VK_SPACE: s = "SPACE"; break;
-    case VK_LEFT: s = "LEFT"; break;
-    case VK_UP: s = "UP"; break;
-    case VK_RIGHT: s = "RIGHT"; break;
-    case VK_DOWN: s = "DOWN"; break;
-    case VK_DELETE: s = "DELETE"; break;
-    case VK_OEM_1: s = "COLON"; break;
-    case VK_OEM_PLUS: s = "EQUALS"; break;
-    case VK_OEM_COMMA: s = "COMMA"; break;
-    case VK_OEM_MINUS: s = "MINUS"; break;
-    case VK_OEM_PERIOD: s = "PERIOD"; break;
-    case VK_OEM_2: s = "SLASH"; break;
-    case VK_OEM_3: s = "TILDE"; break;
-    case VK_OEM_4: s = "LBRACKET"; break;
-    case VK_OEM_5: s = "BACKSLASH"; break;
-    case VK_OEM_6: s = "RBRACKET"; break;
-    case VK_OEM_7: s = "QUOTE"; break;
-    default: s = "OTHER"; break; }
-  if(wParam >= '0' && wParam <= 'Z') s = (char)wParam;
-  return s; }
-
-LRESULT CALLBACK _win_proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
-  POINT p;
-  GetCursorPos(&p);
-  edit.mouse_pos = point(p.x, p.y);
-  RECT r;
-  switch(uMsg){
-  case WM_DESTROY:
-    PostQuitMessage(0);
-    return 0;
-  case WM_PAINT:
-    _win_paint(hwnd);
-    return 0;
-  case WM_MOVE:
-  case WM_SIZE:
-    GetWindowRect(hwnd, &r);
-    edit.win_pos = point(r.left, r.top);
-    edit.width = r.right - r.left, edit.height = r.bottom - r.top;
-    return 0;
-  case WM_MOUSEMOVE:
-    return 0;
-  case WM_LBUTTONDOWN:
-  case WM_LBUTTONUP:
-    edit.process_key("LCLICK", uMsg == WM_LBUTTONDOWN, point(p.x, p.y));
-    return 0;
-  case WM_RBUTTONDOWN:
-  case WM_RBUTTONUP:
-    edit.process_key("RCLICK", uMsg == WM_RBUTTONDOWN, point(p.x, p.y));
-    return 0;
-  case WM_MBUTTONDOWN:
-  case WM_MBUTTONUP:
-    edit.process_key("MCLICK", uMsg == WM_MBUTTONDOWN, point(p.x, p.y));
-    return 0;
-  case WM_MOUSEWHEEL:
-    //!
-    return 0;
-  case WM_KEYDOWN:
-  case WM_KEYUP:
-    edit.process_key(_win_key(wParam), uMsg == WM_KEYDOWN, point(p.x, p.y));
-    return 0;
-  default:
-    return DefWindowProc(hwnd, uMsg, wParam, lParam); } }
+  void add_char(const char c); };
 
 void Editor::init(const HINSTANCE wp1, const int wp2){
+  _win = this;
+
   win_param_1 = wp1, win_param_2 = wp2;
-  width = INIT_WIN_W, height = INIT_WIN_H;
-  last_update = 0;
   updated = true;
+  width = INIT_WIN_W, height = INIT_WIN_H;
+
+  last_update = 0;
   refresh_line = refresh_page = false;
   shift = ctrl = alt = false;
   bkgd_color = INIT_BKGD_COLOR;
@@ -167,25 +72,7 @@ void Editor::init(const HINSTANCE wp1, const int wp2){
   cursor.last_update = 0; }
 
 void Editor::run(){
-  const wchar_t CLASS[] = L"WindowClass";
-  WNDCLASS wc = {};
-  wc.lpfnWndProc = _win_proc;
-  wc.hInstance = win_param_1;
-  wc.lpszClassName = CLASS;
-  RegisterClass(&wc);
-  HWND hwnd = CreateWindowEx(
-      0, CLASS, L"Window", WS_OVERLAPPEDWINDOW, 0, 0,
-      width, height, NULL, NULL, win_param_1, NULL);
-  assert(hwnd != NULL, "window.display", "could not create window");
-  ShowWindow(hwnd, win_param_2);
-  MSG msg = {};
-  while(GetMessage(&msg, NULL, 0, 0)){
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-    double ms = (double)(clock() - last_update) * 1000.0 / CLOCKS_PER_SEC;
-    update(ms);
-    draw();
-    InvalidateRect(hwnd, NULL, FALSE); } }
+  _win_run(); }
 
 void Editor::update(const double ms){
   cursor.pos = point(cursor.x * CHAR_WIDTH, cursor.y * LINE_HEIGHT);
---
+++ edit/window.hh
@@ -0,0 +1,139 @@
+// ABSTRACT EDITOR WINDOW
+
+#ifndef WINDOW_HH
+#define WINDOW_HH
+
+#include "../gl/image.hh"
+
+struct window {
+
+  HINSTANCE win_param_1;
+  int win_param_2;
+
+  bool updated;
+  int width, height;
+  clock_t last_update;
+  point win_pos, mouse_pos;
+  image frame;
+
+  virtual void update(const double ms) = 0;
+  virtual void draw() = 0;
+  virtual void process_key(
+      const str& key, const bool down, const point& mouse) = 0; };
+
+// GLOBAL WINDOW POINTER: SET IN EDITOR INIT
+window* _win;
+
+void _win_paint(HWND hwnd){
+  if(!_win->updated) return;
+  PAINTSTRUCT ps;
+  HDC hdc = BeginPaint(hwnd, &ps);
+  HBITMAP bmp = image_to_bmp(hdc, &_win->frame);
+  HDC hdcMem = CreateCompatibleDC(NULL);
+  HBITMAP bmpPrev = (HBITMAP)SelectObject(hdcMem, bmp);
+  BitBlt(hdc, 0, 0, _win->width, _win->height, hdcMem, 0, 0, SRCCOPY);
+  SelectObject(hdcMem, bmpPrev);
+  DeleteDC(hdcMem);
+  EndPaint(hwnd, &ps);
+  _win->updated = false; }
+
+str _win_key(WPARAM wParam){
+  str s;
+  switch(wParam){
+    // Mouse clicks not handled like this:
+    // case VK_LBUTTON: s = "LCLICK"; break;
+    // case VK_RBUTTON: s = "RCLICK"; break;
+    // case VK_MBUTTON: s = "MCLICK"; break;
+    case VK_BACK: s = "BACKSPACE"; break;
+    case VK_TAB: s = "TAB"; break;
+    case VK_RETURN: s = "ENTER"; break;
+    case VK_SHIFT: s = "SHIFT"; break;
+    case VK_CONTROL: s = "CONTROL"; break;
+    case VK_MENU: s = "ALT"; break;
+    case VK_CAPITAL: s = "CAPSLOCK"; break;
+    case VK_ESCAPE: s = "ESCAPE"; break;
+    case VK_SPACE: s = "SPACE"; break;
+    case VK_LEFT: s = "LEFT"; break;
+    case VK_UP: s = "UP"; break;
+    case VK_RIGHT: s = "RIGHT"; break;
+    case VK_DOWN: s = "DOWN"; break;
+    case VK_DELETE: s = "DELETE"; break;
+    case VK_OEM_1: s = "COLON"; break;
+    case VK_OEM_PLUS: s = "EQUALS"; break;
+    case VK_OEM_COMMA: s = "COMMA"; break;
+    case VK_OEM_MINUS: s = "MINUS"; break;
+    case VK_OEM_PERIOD: s = "PERIOD"; break;
+    case VK_OEM_2: s = "SLASH"; break;
+    case VK_OEM_3: s = "TILDE"; break;
+    case VK_OEM_4: s = "LBRACKET"; break;
+    case VK_OEM_5: s = "BACKSLASH"; break;
+    case VK_OEM_6: s = "RBRACKET"; break;
+    case VK_OEM_7: s = "QUOTE"; break;
+    default: s = "OTHER"; break; }
+  if(wParam >= '0' && wParam <= 'Z') s = (char)wParam;
+  return s; }
+
+LRESULT CALLBACK _win_proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+  POINT p;
+  GetCursorPos(&p);
+  _win->mouse_pos = point(p.x, p.y);
+  RECT r;
+  switch(uMsg){
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    return 0;
+  case WM_PAINT:
+    _win_paint(hwnd);
+    return 0;
+  case WM_MOVE:
+  case WM_SIZE:
+    GetWindowRect(hwnd, &r);
+    _win->win_pos = point(r.left, r.top);
+    _win->width = r.right - r.left, _win->height = r.bottom - r.top;
+    return 0;
+  case WM_MOUSEMOVE:
+    return 0;
+  case WM_LBUTTONDOWN:
+  case WM_LBUTTONUP:
+    _win->process_key("LCLICK", uMsg == WM_LBUTTONDOWN, point(p.x, p.y));
+    return 0;
+  case WM_RBUTTONDOWN:
+  case WM_RBUTTONUP:
+    _win->process_key("RCLICK", uMsg == WM_RBUTTONDOWN, point(p.x, p.y));
+    return 0;
+  case WM_MBUTTONDOWN:
+  case WM_MBUTTONUP:
+    _win->process_key("MCLICK", uMsg == WM_MBUTTONDOWN, point(p.x, p.y));
+    return 0;
+  case WM_MOUSEWHEEL:
+    //!
+    return 0;
+  case WM_KEYDOWN:
+  case WM_KEYUP:
+    _win->process_key(_win_key(wParam), uMsg == WM_KEYDOWN, point(p.x, p.y));
+    return 0;
+  default:
+    return DefWindowProc(hwnd, uMsg, wParam, lParam); } }
+
+void _win_run(){
+  const wchar_t CLASS[] = L"WindowClass";
+  WNDCLASS wc = {};
+  wc.lpfnWndProc = _win_proc;
+  wc.hInstance = _win->win_param_1;
+  wc.lpszClassName = CLASS;
+  RegisterClass(&wc);
+  HWND hwnd = CreateWindowEx(
+      0, CLASS, L"Window", WS_OVERLAPPEDWINDOW, 0, 0,
+      _win->width, _win->height, NULL, NULL, _win->win_param_1, NULL);
+  assert(hwnd != NULL, "window.display", "could not create window");
+  ShowWindow(hwnd, _win->win_param_2);
+  MSG msg = {};
+  while(GetMessage(&msg, NULL, 0, 0)){
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+    double ms = (double)(clock() - _win->last_update) * 1000.0 / CLOCKS_PER_SEC;
+    _win->update(ms);
+    _win->draw();
+    InvalidateRect(hwnd, NULL, FALSE); } }
+
+#endif
